8d547d9293d742a7607f04edac01d4c89e31f186
mkloubert <generalpd@gmx.de>
2013-08-22 00:03:29 +0200

    Android
    - ADD: source code of Android article

---
+++ Android/FullScreenActivity.java
@@ -0,0 +1,13 @@
+public class FullScreenActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+ 
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+                                  WindowManager.LayoutParams.FLAG_FULLSCREEN);
+ 
+        // Parameter muss ggf. angepasst werden
+        this.setContentView(R.layout.main);
+    }
+}
