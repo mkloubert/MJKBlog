289bb3735fd69f6bc1739aab8cf1301075c8d7a5
mkloubert <generalpd@gmx.de>
2013-08-22 20:42:08 +0200

    SynchronizedObservableCollection<T>
    - ADD: thread safe version of ObservableCollection<T>

---
+++ MarcelJoachimKloubert.Blog.Net35/ComponentModel/SynchronizedObservableCollection.cs
@@ -0,0 +1,164 @@
+﻿// s. http://blog.marcel-kloubert.de
+
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace MarcelJoachimKloubert.Blog.ComponentModel
+{
+    /// <summary>
+    /// Eine Thread-sichere <see cref="ObservableCollection{T}" />,
+    /// </summary>
+    /// <typeparam name="T">Typ der Elemente.</typeparam>
+    public class SynchronizedObservableCollection<T> : ObservableCollection<T>
+    {
+        #region Constructors (4)
+
+        /// <summary>
+        /// Initialisiert eine neue Instanz der
+        /// Klasse <see cref="SynchronizedObservableCollection{T}"/>.
+        /// </summary>
+        /// <param name="syncRoot">Das Objekt für Thread-sichere Operationen.</param>
+        /// <param name="collection">Die Elemente, die von Anfang an Teil dieser List sein sollen.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="syncRoot" /> und/oder <paramref name="collection" />
+        /// ist eine <see langword="null" /> Referenz.
+        /// </exception>
+        public SynchronizedObservableCollection(object syncRoot, IEnumerable<T> collection)
+            : base(collection: collection)
+        {
+            if (syncRoot == null)
+            {
+                throw new ArgumentNullException("syncRoot");
+            }
+
+            this.SyncRoot = syncRoot;
+        }
+
+        /// <summary>
+        /// Initialisiert eine neue Instanz der
+        /// Klasse <see cref="SynchronizedObservableCollection{T}"/>.
+        /// </summary>
+        /// <param name="syncRoot">Das Objekt für Thread-sichere Operationen.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="syncRoot" /> ist eine <see langword="null" /> Referenz.
+        /// </exception>
+        public SynchronizedObservableCollection(object syncRoot)
+            : base()
+        {
+            if (syncRoot == null)
+            {
+                throw new ArgumentNullException("syncRoot");
+            }
+
+            this.SyncRoot = syncRoot;
+        }
+
+        /// <summary>
+        /// Initialisiert eine neue Instanz der
+        /// Klasse <see cref="SynchronizedObservableCollection{T}"/>.
+        /// </summary>
+        /// <param name="collection">Die Elemente, die von Anfang an Teil dieser List sein sollen.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="collection" /> ist eine <see langword="null" /> Referenz.
+        /// </exception>
+        public SynchronizedObservableCollection(IEnumerable<T> collection)
+            : this(syncRoot: new object(),
+                   collection: collection)
+        {
+
+        }
+
+        /// <summary>
+        /// Initialisiert eine neue Instanz der
+        /// Klasse <see cref="SynchronizedObservableCollection{T}"/>.
+        /// </summary>
+        public SynchronizedObservableCollection()
+            : this(syncRoot: new object())
+        {
+
+        }
+
+        #endregion Constructors
+
+        #region Properties (1)
+
+        /// <summary>
+        /// Gets the object for thread safe operations.
+        /// </summary>
+        public object SyncRoot
+        {
+            get;
+            private set;
+        }
+
+        #endregion Properties
+
+        #region Methods (5)
+
+        // Protected Methods (5) 
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <see cref="ObservableCollection{T}.ClearItems()" />
+        protected override void ClearItems()
+        {
+            lock (this.SyncRoot)
+            {
+                base.ClearItems();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <see cref="ObservableCollection{T}.InsertItem(int, T)" />
+        protected override void InsertItem(int index, T item)
+        {
+            lock (this.SyncRoot)
+            {
+                base.InsertItem(index, item);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <see cref="ObservableCollection{T}.MoveItem(int, int)" />
+        protected override void MoveItem(int oldIndex, int newIndex)
+        {
+            lock (this.SyncRoot)
+            {
+                base.MoveItem(oldIndex, newIndex);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <see cref="ObservableCollection{T}.RemoveItem(int)" />
+        protected override void RemoveItem(int index)
+        {
+            lock (this.SyncRoot)
+            {
+                base.RemoveItem(index);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <see cref="ObservableCollection{T}.SetItem(int, T)" />
+        protected override void SetItem(int index, T item)
+        {
+            lock (this.SyncRoot)
+            {
+                base.SetItem(index, item);
+            }
+        }
+
+        #endregion Methods
+    }
+}
