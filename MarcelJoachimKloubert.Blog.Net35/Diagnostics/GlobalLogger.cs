e470c5765d8aff5d2b3620290b13b3694a2dd956
mkloubert <generalpd@gmx.de>
2013-10-12 23:56:08 +0200

    logger
    - ADD: global logger class

---
+++ MarcelJoachimKloubert.Blog.Net35/Diagnostics/GlobalLogger.cs
@@ -0,0 +1,46 @@
+﻿// s. http://blog.marcel-kloubert.de
+
+
+using System;
+using MarcelJoachimKloubert.Blog.Diagnostics;
+
+
+/// <summary>
+/// Klasse zum globalen Zugriff auf eine <see cref="ILoggerFacade" />.
+/// </summary>
+public static class GlobalLogger
+{
+    #region Fields (1)
+
+    private static Func<ILoggerFacade> _provider;
+
+    #endregion Fields
+
+    #region Properties (1)
+
+    /// <summary>
+    /// Gibt den aktuellen logger zurück.
+    /// </summary>
+    public static ILoggerFacade Current
+    {
+        get { return _provider(); }
+    }
+
+    #endregion Properties
+
+    #region Methods (1)
+
+    // Public Methods (1) 
+
+    /// <summary>
+    /// Definiert die Funktion, die den Wert für die
+    /// <see cref="GlobalLogger.Current" /> Eigenschaft zurückgibt.
+    /// </summary>
+    /// <param name="newProvider">Die neue Funktion.</param>
+    public static void SetLoggerProvider(Func<ILoggerFacade> newProvider)
+    {
+        _provider = newProvider;
+    }
+
+    #endregion Methods
+}
